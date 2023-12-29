b369cd5ab00e2683155d98f3d8483b53fed96d97
Armando2030 <armandogs2030@gmail.com>
2023-12-29 16:00:40 -0600

    model usuario

---
+++ back-end/RescueMarket/RescueMarket/Model/Usuario.cs
@@ -0,0 +1,14 @@
+﻿namespace RescueMarket.Model
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string NombreUsuario { get; set; }
+        public string CorreoElectronico { get; set; }
+        public string Contrasena { get; set; }
+        public string ApellidoPaterno { get; set; }
+        public string ApellidoMaterno { get; set; }
+        public string Nombre { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+    }
+}
