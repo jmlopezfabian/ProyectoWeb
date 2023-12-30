6f171d14b7163fdfadfbc1a677fd00a5157df1c1
Armando2030 <armandogs2030@gmail.com>
2023-12-30 00:38:09 -0600

    ProductoController

---
+++ back-end/RescueMarket/RescueMarket/Controllers/ProductoController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RescueMarket.Context;
+using RescueMarket.Model;
+
+namespace RescueMarket.Controllers
+{
+    [Route ("Productos")]
+    [ApiController]
+    public class ProductoController : ControllerBase
+    {
+        [HttpGet]
+        public JsonResult ProductoGet()
+        {
+            List<Producto> productos = new List<Producto>();
+            using (RMContext context = new RMContext())
+            {
+                var aux = context.Productos;
+                foreach(var item in aux)
+                {
+                    productos.Add(new Producto
+                    {
+                        ID_producto = item.ID_producto,
+                        Nombre_Producto = item.Nombre_Producto,
+                        Descripcion = item.Descripcion,
+                        Precio = item.Precio
+                    });
+                }
+            }
+            return new JsonResult(productos);
+        }
+        [HttpPost]
+        public JsonResult ProductoPost([FromBody] Producto productos)
+        {
+            bool flag = false;
+            using (RMContext context = new RMContext())
+            {
+                context.Productos.Add(productos);
+                context.SaveChanges();
+                flag = true;
+            }
+            return new JsonResult(flag);
+        }
+        [HttpPatch]
+        public JsonResult UpdateProducto([FromBody] Producto productos)
+        {
+            bool flag = false;
+            using (RMContext context = new RMContext())
+            {
+                var existe = context.Productos.SingleOrDefault(a => a.ID_producto == productos.ID_producto);
+                if (existe != null)
+                {
+                    context.Entry(existe).State = EntityState.Detached;
+                    context.Productos.Attach(productos);
+                    context.Entry(productos).State = EntityState.Modified;
+                    context.SaveChanges();
+                    flag = true;
+                }
+            }
+            return new JsonResult(flag);
+        }
+        [HttpDelete]
+        public JsonResult DeleteProducto([FromBody] Producto productos)
+        {
+            bool flag = false;
+            using (RMContext context = new RMContext())
+            {
+                var existe = context.Productos.SingleOrDefault(a => a.ID_producto == productos.ID_producto);
+                if (existe != null)
+                {
+                    context.Entry(existe).State = EntityState.Detached;
+                    context.Productos.Attach(productos);
+                    context.Entry(productos).State = EntityState.Deleted;
+                    context.SaveChanges();
+                    flag = true;
+                }
+            }
+            return new JsonResult(flag);
+        } 
+    }
+}
