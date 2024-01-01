e3984e615c9eaf18c5de00018fa21eff76fdb244
jesus <jmlopezfabian@gmail.com>
2023-12-31 19:28:22 -0600

    Cambiando colores y añadiendo fotos integrantes

--- front-end/ProyectoWeb/tailwind.config.js
+++ front-end/ProyectoWeb/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        'principal': '#00457F'
+      }
+    },
   },
   plugins: [],
 }
